ca76081b9aa02cbc2db13df41c64d235385d044a
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-07 20:01:22 -0500

    add seo and mobile responsive

---
+++ app/models/seo.rb
@@ -0,0 +1,4 @@
+class Seo
+  Title = "Tailwind Genius"
+  Description = "AI powered custom Tailwindcss components. Enter a prompt and create a tailwind component you can copy right into your app."
+end
