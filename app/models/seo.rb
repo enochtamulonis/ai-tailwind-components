665409e357d0d482f105a371ab80719bd814f0ad
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-08 22:11:16 -0500

    add seo imporvement

--- app/models/seo.rb
+++ app/models/seo.rb
@@ -1,4 +1,7 @@
 class Seo
   Title = "Tailwind Genius"
-  Description = "AI powered custom Tailwindcss components. Enter a prompt and create a tailwind component you can copy right into your app."
+  Description = "
+  AI powered custom Tailwindcss components. Enter a prompt and create a tailwind component you can copy right into your app.
+  ai generated tailwind components for any website using tailwindcss. Its never been easier to make tailwindcss components with our tool.
+  "
 end
