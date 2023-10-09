1c48ff904313faf915087214b23610d7a0bf9a60
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-09 13:52:10 -0500

    fix cancel controller

--- app/controllers/subscriptions/cancel_controller.rb
+++ app/controllers/subscriptions/cancel_controller.rb
@@ -1,9 +1,11 @@
-class Subscription::CancelController < ApplicationController
-  def create
-    subscription = current_user.subscription
-    Stripe::Subscription.delete(subscription.stripe_subscription_id)
-    if subscription.destroy!
-      redirect_to root_path, notice: "Your subscription was cancelled"
+module Subscriptions
+  class CancelController < ApplicationController
+    def create
+      subscription = current_user.subscription
+      Stripe::Subscription.delete(subscription.stripe_subscription_id)
+      if subscription.destroy!
+        redirect_to root_path, notice: "Your subscription was cancelled"
+      end
     end
   end
 end
