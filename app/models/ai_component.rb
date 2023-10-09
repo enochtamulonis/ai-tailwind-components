8f0eec678726d9a23060b1ec298338a16140e694
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-08 20:29:34 -0500

    add solution for compiling classes fixes some bugs i was seeing and dindt even realize

--- app/models/ai_component.rb
+++ app/models/ai_component.rb
@@ -5,6 +5,17 @@ class AiComponent < ApplicationRecord
     if ai_prompt_service.success
       pieces = ai_prompt_service.results.split("```html").last
       html = pieces.split("```")[0].gsub("`", "")
+      # Save the file locally so tailwind compile loads classes for development
+      if Rails.env.development?
+        file_path = Rails.root.join('tmp', "classes", "tailwind-component-#{SecureRandom.hex(6)}.txt")
+
+        File.open(file_path, 'w') do |file|
+          # Add text to the file
+          file.puts(html)
+        end
+        puts "SAVED THE FILE"
+      end
+
       update(html_content: html, ai_results: ai_prompt_service.results)
     end
   end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './tmp/classes/*'
   ],
   theme: {
     extend: {
@@ -19,5 +20,11 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
+  ],
+  // Enable when deploy to prod.
+  // safelist: [
+  //   {
+  //     pattern: /./, // the "." means "everything"
+  //   },
+  // ],
 }
