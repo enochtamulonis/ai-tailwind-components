41cb874340d1c686517d5b52082b4b8f57188bf7
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-09 14:50:04 -0500

    seo improvements

--- app/models/ai_prompt.rb
+++ app/models/ai_prompt.rb
@@ -8,8 +8,8 @@ class AiPrompt
   def call
       client = OpenAI::Client.new
       question = "Make me a tailwindcss component #{ai_prompt}. "
-      question += "Only use icons if needed. Get them from heroicons and make sure they look correct"
-      question += "only return the html don't include any javascript code or cdns"
+      question += "Only use icons if needed. Get them from heroicons and make sure they look correct "
+      question += "only return the html don't include any javascript code or cdns or css"
       response = client.chat(
           parameters: { 
               model: "gpt-3.5-turbo", # Required.
