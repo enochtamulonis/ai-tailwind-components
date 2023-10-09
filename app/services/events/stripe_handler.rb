99b83165d96a916e66573feb513f623782a6c254
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-09 16:20:17 -0500

    fix subscription code

--- app/services/events/stripe_handler.rb
+++ app/services/events/stripe_handler.rb
@@ -4,19 +4,21 @@ module Events
       stripe_event = Stripe::Event.construct_from(event.data)
       puts "Processing stripe event #{stripe_event.type}"
       case stripe_event.type
-        when 'customer.subscription.created'
-          # Create subscription update user
+      when 'customer.subscription.updated'
+        # Create subscription update user
+        if stripe_event.data.object.status == "active"
           metadata = stripe_event.data.object.metadata
           user = User.find(metadata.user_id)
           subscription_id = stripe_event.data.object.id
           subscription = Subscription.find_or_create_by(user: user, stripe_subscription_id: subscription_id)
           subscription.active!
           user.broadcast_replace_to(user, partial: "subscriptions/success/success", target: ActionView::RecordIdentifier.dom_id(user, :success))
-        when 'invoice.payment_failed'
-          # Inform the user of failed payment
-        when 'customer.subscription.deleted'
-          # Do something when deleted
         end
+      when 'invoice.payment_failed'
+      # Inform the user of failed payment
+      when 'customer.subscription.deleted'
+        # Do something when deleted
+      end
     end
   end
 end
