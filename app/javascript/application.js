fe725134f2c5e0d31e149348755e83d1f331e25d
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-09 15:03:42 -0500

    add google analytics

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", (event) => {
+  let trackingId = document.querySelector("meta[name='google-analytics-tracking-id']").content
+  if (trackingId) {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', trackingId);    
+  }
+})
