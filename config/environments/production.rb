4a5c26eb654031dc9d8e37a04f219a35dc06a211
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-10 22:28:12 -0500

    remove the http from the url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,12 +68,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://www.tailwind-genius.com' }
+  config.action_mailer.default_url_options = { host: 'www.tailwind-genius.com' }
 
   ActionMailer::Base.smtp_settings = {
     :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
     :password => Rails.application.credentials.dig(:sendgrid, :api_key), # This is the secret sendgrid API key which was issued during API key creation
-    :domain => 'https://www.tailwind-genius.com',
+    :domain => 'www.tailwind-genius.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
