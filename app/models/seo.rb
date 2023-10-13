523aa54e70679a888a6596b87c969baf69f4293d
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-13 11:15:45 -0500

    update seo

--- app/models/seo.rb
+++ app/models/seo.rb
@@ -1,7 +1,7 @@
 class Seo
   Title = "Tailwind Genius"
   Description = "
-  AI powered custom Tailwindcss components. Enter a prompt and create a tailwind component you can copy right into your app.
-  ai generated tailwind components for any website using tailwindcss. Its never been easier to make tailwindcss components with our tool.
+  AI powered custom Tailwindcss components. Easily create tailwind components without writing any code, then you can copy them right into your app.
+  We help you create AI generated tailwind components for any website. Its never been easier to make beautiful UI with our tool.
   "
 end
