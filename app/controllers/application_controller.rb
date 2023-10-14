a6bd05ac1601baa5f6bfb3ac6038d198772afbcd
Enoch Tamulonis <enochtamulonis@gmail.com>
2023-10-14 12:00:32 -0500

    fix the broafcast hahah erro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_guest_token, unless: :current_user
+
+  def set_guest_token
+    if !session[:guest_token].present?
+      session[:guest_token] = SecureRandom.hex(6)
+    end
+    @guest_token ||= session[:guest_token]
+  end
 end
