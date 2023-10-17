524acf030167b5a2feb3e9f538f3b876cdfce042
enoch tamulonis <yungboiiindigo@gmail.com>
2023-10-17 12:38:05 -0500

    fix unsubscribe url

--- app/mailers/admin_email_mailer.rb
+++ app/mailers/admin_email_mailer.rb
@@ -1,7 +1,9 @@
 class AdminEmailMailer < ApplicationMailer
+  include Rails.application.routes.url_helpers
   def send_email(email, user)
     @email = email
     @user = user
+    @unsubscribe_url = user_unsubscribe_email_url(user.email_token)
     name = @user.full_name || "User"
     @body = @email.body.gsub("<name>", name)
     mail({
--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,6 +1,8 @@
 class WelcomeMailer < ApplicationMailer
+  include Rails.application.routes.url_helpers
   def send_welcome_email(user)
     @user = user
+    @unsubscribe_url = user_unsubscribe_email_url(user.email_token)
     mail({
       to: user.email,
       subject: 'Welcome to the family! Thanks for joining'
