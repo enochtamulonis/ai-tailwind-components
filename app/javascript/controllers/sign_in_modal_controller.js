a347b7019c33ca10f0ffc2773aff5a55d80f8c6d
enoch tamulonis <yungboiiindigo@gmail.com>
2023-10-19 14:27:01 -0500

    ui updates

--- app/javascript/controllers/sign_in_modal_controller.js
+++ app/javascript/controllers/sign_in_modal_controller.js
@@ -1,16 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
-import { post } from "@rails/request.js"
+import { Controller } from "@hotwired/stimulus";
+import { post } from "@rails/request.js";
 // Connects to data-controller="sign-in-modal"
 export default class extends Controller {
   connect() {
     this.timeout = setTimeout(async () => {
-      await post("/sign_in_modal")
-    }, 7500) 
+      await post("/sign_in_modal");
+    }, 10000);
   }
   disconnect() {
     if (this.timeout) {
-      clearTimeout(this.timeout)
+      clearTimeout(this.timeout);
     }
-  }  
+  }
 }
-
